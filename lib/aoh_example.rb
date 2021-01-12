868695822d05f985f140a85dd716106fd52e332e
lindsayMecher <lindsaymecher@gmail.com>
2021-01-12 12:43:29 -0600

    commit tree adf8d73041a6641b7019a455919bcc2f94b0fb0b

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,3 +1,4 @@
+  
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
@@ -7,24 +8,20 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  [
-    DON_G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G
-    ]
+  # Build an array that contains (or, "nests") the constants into a single
+  # Array. Ruby constants are denoted by ALL_CAPS
+  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
 def literal_aoh
-  [
-    {name:  "Don Gately", occupation: "Live-in Staff"},
+  # Using Array literal syntax only, build a nested array that uses the data in
+  # held in the constants
+  [{ name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
-    
-    ]
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
@@ -32,8 +29,8 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh[row][key] = new_value
-  aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  aoh[row][key] = new_value 
+  aoh
 end
